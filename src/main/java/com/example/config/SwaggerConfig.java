ab0d0d234d20239468fba98dc86ac80b38c05fa5
Firdavs <firdavsamonov1998>
2023-04-07 16:17:46 +0500

    Auditing added

--- src/main/java/com/example/config/SwaggerConfig.java
+++ src/main/java/com/example/config/SwaggerConfig.java
@@ -1,6 +1,4 @@
 package com.example.config;
-
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
